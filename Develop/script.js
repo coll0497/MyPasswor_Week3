292f61230fa8a3961c0f0c38a3da45679e0ca109
Chris C <coll0497@gmail.com>
2021-08-14 22:01:30 -0500

    Password generator displaying

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,10 +11,16 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
-// Generator Functions - http://ww.net-comber.com/charset.html
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(Math.floor(Math.random() * 26) + 97)
+ let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*(){}[]+<>?,.";
+
+ let password = "";
+ for(var i = 1; i <= 15; i++){
+   password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+ }
+
+console.log(password)
+
+
+;
